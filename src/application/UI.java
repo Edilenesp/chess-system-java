d660ba708541bf0133bbf8ae6285daf9bba817a9
Edilene do Amaral Oliveira <edilenesp@gmail.com>
2022-07-24 09:51:11 -0300

    Handling captured pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import chess.ChessMatch;
 import chess.ChessPiece;
@@ -49,8 +52,10 @@ public class UI {
 		}            //InputMismatchException que é uma exceção que já tem no java que é um erro de entrada de dados
 	}
 	
-	public static void printMatch(ChessMatch chessMach) {
+	public static void printMatch(ChessMatch chessMach, List<ChessPiece> captured) {
 		printBoard(chessMach.getPieces());
+		System.out.println();
+		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn: " + chessMach.getTurn());
 		System.out.println("Waiting player: " + chessMach.getCurrentPlayer());
@@ -94,6 +99,20 @@ public class UI {
             }
         }
         System.out.print(" ");
+	}
+	
+	private static void printCapturedPieces(List<ChessPiece> captured) {
+		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+		System.out.println("Captured pieces: ");
+		System.out.print("White: ");
+		System.out.print(ANSI_WHITE);
+		System.out.println(Arrays.toString(white.toArray()));
+		System.out.print(ANSI_RESET);
+		System.out.print("Black: ");
+		System.out.print(ANSI_YELLOW);
+		System.out.println(Arrays.toString(black.toArray()));
+		System.out.print(ANSI_RESET);
 	}
 
 
